ecb57df2d035f1bb7c9adc41f3474dd54fca2c12
Sougata Das <sougata19582001@gmail.com>
2022-07-15 21:18:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+# create a flask instance
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# create custom Error pages
+# invalid Url
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# internal server error
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
