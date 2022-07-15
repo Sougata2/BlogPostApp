e53997009d4fbe97aad3491fe15ea34a0ade6679
Sougata Das <sougata19582001@gmail.com>
2022-07-16 01:22:41 +0530

    error files and name file added

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-# create a flask instance
+
+# initializations
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 
+# Forms
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?",
+                       validators=[DataRequired(message="Your Name")])
+    submit = SubmitField("Submit")
+
+
+# Views
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    form = NamerForm()
+    name = None
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', form=form, name=name)
 
 
 # create custom Error pages
